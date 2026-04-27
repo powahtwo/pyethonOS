53c929663474a2ebc1a9f6ddaba83b2bb9e1aed3
Powahkat <droidwarrior8@gmail.com>
2026-04-27 14:48:04 -0700

    Update pyethonOS.py

--- pyethonOS.py
+++ pyethonOS.py
@@ -1,7 +1,6 @@
 import random
-import cursor
-helptext = "commands are help, ver, notepad, infinity, clitest, changelog, dice"
-# this is what the user types on. \n is a new line
+helptext = "commands are help, ver, notepad, infinity, changelog, dice, calculator"
+# this is a the user types on. \n is a new line
 cmd = input("|Type a command type help for cmds|\n▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪\n► ")
 if cmd not in helptext:
     print("invalid command")
@@ -9,7 +8,7 @@ elif "help" in cmd:
   print(helptext)
   print("re-run program to type new command↺ ")
 elif "ver" in cmd:
-  print("pyethonOS 1.0.1")
+  print("pyethonOS 1.2")
 elif "notepad" in cmd:
     while True:
      text = input("| ")
@@ -18,15 +17,28 @@ elif "infinity" in cmd:
     while True:
      print("This will run forever.")
 elif "changelog" in cmd:
-    print("bug fixes\n ❒removed calculator,qmight be back soon")
+    print("bug fixes\n ❒added calculator")
 elif "commands" in cmd:
     print("invalid command")
 elif "are" in cmd:
     print("invalid command")
-elif "clitest" in cmd:
-	cursor.hide()
-	cursor.show()
 elif "dice" in cmd:
     print("you rolled a...")
     print(random.randrange(1, 6))
-    
+elif "calculator" in cmd:
+    print((lambda n1, op, n2: n1 + n2 if op == "1" else n1 - n2 if op == "2" else n1 * n2 if op == "3" else n1 / n2 if op == "4" else "Invalid")(float(input("type 1st number: ")), input("type 1 for add\ntype 2 for subtract\ntype 3 for multiply\ntype 4 for divide: "), float(input("type 2nd number: "))))
+
+"""
+ the old calculator code
+    num1 = float(input("type 1st number: ")) 
+calc1 = input("type 1 for add\ntype 2 for subtract\ntype 3 for multiply\ntype 4 for divide: ")
+num2 = float(input("type 2nd number: "))
+if calc1 == "1":
+    print(num1 + num2)
+elif calc1 == "2":
+    print(num1 - num2)
+elif calc1 == "3":
+    print(num1 * num2)
+elif calc1 == "4":
+    print(num1 / num2)
+"""
